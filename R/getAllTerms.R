c11c1ed51722d1e1fde462d412721b0ea2037322
Kamil Bartoń <kamil.barton@go2.pl>
2010-09-03 00:00:00 +0000

    version 0.13.14

--- R/getAllTerms.R
+++ R/getAllTerms.R
@@ -1,99 +1,87 @@
 `getAllTerms.default` <-
-function(x, ...) {
-	return(getAllTerms(as.formula(formula(x))))
-}
+function(x, ...) getAllTerms(as.formula(formula(x)), ...)
 
-`getAllTerms.formula` <-
-function(x, ...) {
-	mTerms <- terms(x)
-	intercept <- attr(mTerms, "intercept")
-	if (!is.null(attr(mTerms, "offset"))){
-		offs <-
-		sapply(as.list(attr(mTerms,"variables")[-1])[attr(mTerms,"offset")],
-		deparse)
+
+`getAllTerms.terms` <-
+function(x, offset = TRUE, ...) {
+	if (!is.null(attr(x, "offset"))){
+		offs <- sapply((attr(x, "variables")[-1])[attr(x, "offset")], deparse)
 	} else {
 		offs <- NULL
 	}
-	ret <- attr(mTerms, "term.labels")
-	if (length(ret) > 0) {
-		ret <- ret[order(ret)]
-		i <- grep(" ", ret)
-		ret[i] <- paste("(", ret[i] , ")")
+	ret <- attr(x, "term.labels")
 
-		mTerms <- terms(reformulate(ret))
-		ret <- attr(mTerms, "term.labels")
+	# Get term names, with higher order term components arranged alphabetically
+	if (length(ret) > 0) {
+		factors <- attr(x, "factors")
+		factors1 <- rownames(factors)
+		ret <- apply(factors > 0, 2, function(i) paste(sort(factors1[i]), collapse=":"))
 	}
 
-	attr(ret, "intercept") <- intercept
+	# Leave out random terms (lmer type)
+	#ran <- attr(x, "variables")[-1][-c(attr(x, "offset"), attr(x, "response"))]
+	ran <- attr(x, "variables")[-1]
+	ran <- as.character(ran[sapply(ran,
+		function(x) length(x) == 3 && x[[1]] == as.name("|"))])
+	ifx <- !(ret %in% ran)
 
-	if (!is.null(offs[1]))
+	ret <- ret[ifx]
+
+	# finally, sort by order and then alphabetically
+	ret <- unname(ret[order(attr(x, "order")[ifx], ret)])
+
+
+	if (!is.null(offs[1])) {
+		if (offset)
+			ret <- c(ret, offs)
 		attr(ret, "offset") <- offs
+	}
+	attr(ret, "intercept") <- attr(x, "intercept")
+	if (length(ran) > 0) {
+		attr(ret, "random.terms") <- ran
+		attr(ret, "random") <- reformulate(c(".", paste("(", ran, ")",
+			sep = "")), response = ".")
+	}
+
 	return(ret)
 }
 
-`getAllTerms.glmer` <-
-function(x, ...) {
-     ret <- getAllTerms(as.formula(formula(x)))
-     i <- grep(" \\| ", ret)
 
-     intercept <- attr(ret, "intercept")
 
-	rnd <- ret[i]
-     ret <- ret[-i]
-     attr(ret, "random.terms") <- rnd
-     rnd.formula <- paste("(", rnd, ")", sep="", collapse=" + ")
-     rnd.formula <- as.formula(paste(". ~ .", rnd.formula, sep="+"))
+`getAllTerms.formula` <-
+function(x, ...) getAllTerms.terms(terms(x), ...)
 
-	attr(ret, "random") <- rnd.formula
-	attr(ret, "intercept") <- intercept
-
-     ret
-}
 
 `getAllTerms.lme` <-
 function(x, ...) {
-	getAllTerms(as.formula(formula(x)))
+	ret <- getAllTerms(terms(x))
+	attr(ret, "random") <- . ~ .
+
+	# Code from nlme:::print.reStruct, modified slightly
+	reStruct <- x$modelStruct$reStruct
+	nobj <- length(reStruct)
+	if (is.null(namx <- names(reStruct)))
+		names(reStruct) <- nobj:1
+	aux <- t(array(rep(names(reStruct), nobj), c(nobj, nobj)))
+	aux[lower.tri(aux)] <- ""
+	reStruct[] <- rev(reStruct)
+	aux <- t(array(rep(names(reStruct), nobj), c(nobj, nobj)))
+	aux[lower.tri(aux)] <- ""
+	attr(ret, "random.terms") <- paste(lapply(lapply(reStruct, attr, "formula"),
+		"[[", 2), "|",
+		rev(apply(aux, 1, function(z) paste(z[z != ""], collapse = " %in% "))))
+
+	return(ret)
 }
 
-`getAllTerms.lmer` <-
-function(x, ...) {
-     ret <- getAllTerms(as.formula(formula(x)))
-     i <- grep(" \\| ", ret)
 
-     intercept <- attr(ret, "intercept")
 
-	rnd <- ret[i]
-     ret <- ret[-i]
-     attr(ret, "random.terms") <- rnd
 
-     rnd.formula <- paste("(", rnd, ")", sep="", collapse=" + ")
-     rnd.formula <- as.formula(paste(". ~ .", rnd.formula, sep="+"))
-
-	attr(ret, "random") <- rnd.formula
-	attr(ret, "intercept") <- intercept
-
-     ret
-}
-
+`getAllTerms.glmer` <- # For backwards compatibility
+`getAllTerms.lmer` <-  # with older versions of lme4
 `getAllTerms.mer` <-
-function(x, ...) {
-     ret <- getAllTerms(as.formula(formula(x)))
-     i <- grep(" \\| ", ret)
+function(x, ...) getAllTerms(formula(x), ...)
 
-     intercept <- attr(ret, "intercept")
-
-	rnd <- ret[i]
-     ret <- ret[-i]
-     attr(ret, "random.terms") <- rnd
-
-     rnd.formula <- paste("(", rnd, ")", sep="", collapse=" + ")
-     rnd.formula <- as.formula(paste(". ~ .", rnd.formula, sep="+"))
-
-	attr(ret, "random") <- rnd.formula
-	attr(ret, "intercept") <- intercept
-
-     ret
-}
 
 `getAllTerms` <-
 function(x, ...) UseMethod("getAllTerms")
